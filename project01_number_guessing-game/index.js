d2e462bc2be76d7732ad276b99547ac659f9233d
Noman Abid <nomanabid3557@gmail.com>
2023-08-02 09:11:13 +0500

    project number guessing game updated

--- project01_number_guessing-game/index.js
+++ project01_number_guessing-game/index.js
@@ -1,14 +1,64 @@
-"use strict";
-const input = require('prompt-sync')({ sigint: true });
-const guessNumber = () => {
-    const randomNumber = Math.floor(Math.random() * (10 - 0 + 1) + 0);
-    let guess = 0;
-    guess = parseInt(input('Guess a number between 0 and 10: '));
-    if (guess === randomNumber) {
-        console.log('You guessed correctly!');
-    }
-    else {
-        console.log(`You guessed incorrectly! The number was ${randomNumber}`);
-    }
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import { createSpinner } from "nanospinner";
+const log = console.log;
+const failure = chalk.bold.red;
+const success = chalk.bold.green;
+const numOfTurns = 3;
+const diffiultyLevels = ["easy", "medium", "hard"];
+const getRandomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
-guessNumber();
+const playGame = async (randomNumber, numOfTurns) => {
+    const { guess } = await inquirer.prompt([
+        {
+            name: "guess",
+            type: "number",
+            message: "Guess the number",
+        },
+    ]);
+    if (guess === randomNumber) {
+        log(success("You guessed it right"));
+        log("=====================================");
+        log("Game Over");
+        log("=====================================");
+        chalkAnimation.rainbow("You won");
+        return;
+    }
+    if (numOfTurns === 1) {
+        log(failure("You lost"));
+        log("=====================================");
+        log("Game Over");
+        log("=====================================");
+        chalkAnimation.radar("You lost");
+        return;
+    }
+    log(failure("You guessed it wrong"));
+    log("=====================================");
+    log(`You have ${numOfTurns - 1} turns left`);
+    log("=====================================");
+    playGame(randomNumber, numOfTurns - 1);
+};
+const startGame = async () => {
+    const { difficulty } = await inquirer.prompt([
+        {
+            name: "difficulty",
+            type: "list",
+            message: "Select difficulty level",
+            choices: diffiultyLevels,
+        },
+    ]);
+    const randomNumber = getRandomNumber(1, difficulty === "easy" ? 10 : difficulty === "medium" ? 20 : 50);
+    const spinner = createSpinner("Generating random number");
+    spinner.start();
+    setTimeout(() => {
+        spinner.stop();
+        log(success("Random number generated"));
+        log("You have 3 turns to guess the number");
+        log("Lets start the game");
+        log("=====================================");
+        playGame(randomNumber, numOfTurns);
+    }, 2000);
+};
+startGame();
--- project01_number_guessing-game/index.ts
+++ project01_number_guessing-game/index.ts
@@ -1,18 +1,78 @@
-const input = require('prompt-sync')(
-    {sigint: true}
-)
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import { createSpinner } from "nanospinner";
 
-const guessNumber = () => {
+const log = console.log;
+const failure = chalk.bold.red;
+const success = chalk.bold.green;
 
-    const randomNumber:number = Math.floor(Math.random() * (10-0+1)+0) 
-    let guess:number = 0
+const numOfTurns = 3;
+const diffiultyLevels = ["easy", "medium", "hard"];
 
-    guess = parseInt(input('Guess a number between 0 and 10: '))
+const getRandomNumber = (min:number, max:number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+
+const playGame = async (randomNumber:number, numOfTurns:number) => {
+    const { guess } = await inquirer.prompt([
+        {
+        name: "guess",
+        type: "number",
+        message: "Guess the number",
+        },
+    ]);
+     
     if (guess === randomNumber) {
-        console.log('You guessed correctly!')
-    }else{
-        console.log(`You guessed incorrectly! The number was ${randomNumber}`)
+        log(success("You guessed it right"));
+        log("=====================================");
+        log("Game Over");
+        log("=====================================");
+        chalkAnimation.rainbow("You won");
+        return;
     }
+
+    if (numOfTurns === 1) {
+        log(failure("You lost"));
+        log("=====================================");
+        log("Game Over");
+        log("=====================================");
+        chalkAnimation.radar("You lost");
+        return;
+    }
+
+    log(failure("You guessed it wrong"));
+    log("=====================================");
+    log(`You have ${numOfTurns - 1} turns left`);
+    log("=====================================");
+    playGame(randomNumber, numOfTurns - 1);
 }
 
-guessNumber()
+const startGame = async () => {
+    
+    const { difficulty } = await inquirer.prompt([
+        {
+        name: "difficulty",
+        type: "list",
+        message: "Select difficulty level",
+        choices: diffiultyLevels,
+        },
+    ]);
+    
+    const randomNumber = getRandomNumber(1, difficulty === "easy" ? 10 : difficulty === "medium" ? 20 : 50);
+    const spinner = createSpinner("Generating random number");
+    
+    spinner.start();
+    
+    setTimeout(() => {
+        spinner.stop();
+        log(success("Random number generated"));
+        log("You have 3 turns to guess the number");
+        log("Lets start the game");
+        log("=====================================");
+        playGame(randomNumber, numOfTurns);
+    }, 2000);
+};
+
+startGame();
